d1632dd14a27d902de3e5e30d6ce82fb2ec2b503
Daniele Genta <s236676@studenti.polito.it>
2019-05-08 10:28:35 +0200

    layout

--- src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -1,5 +1,52 @@
 package it.polito.tdp.dizionariograph;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class DizionarioGraphController {
-	
+
+    @FXML
+    private TextField txtNumeroLettere;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private Button btnGeneraGrafo;
+
+    @FXML
+    private Button btnTrovaVicini;
+
+    @FXML
+    private Button btnTrovaGradoMax;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doGeneraGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTrovaGradoMax(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTrovaVicini(ActionEvent event) {
+
+    }
+
 }
